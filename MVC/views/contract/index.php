624029ea3e50b5561a00c8f39004ac8e310eb42b
fenfen <liufen2004@hotmail.com>
2018-10-28 12:50:43 -0400

    vue fournisseur modal
    le détail marche, mais le button close ne fonctionne pas

--- MVC/views/contract/index.php
+++ MVC/views/contract/index.php
@@ -1,6 +1,8 @@
 <head>
     <title>Liste des contrats/projets</title>
 </head>
+<body>
+<div class="container" style="margin-top:50px;">
 
 <div class="row cont-titre-search">
     <div class="col-xs-12 col-md-6 col-lg-6" style="margin:0">
@@ -19,7 +21,7 @@
 
 <!--Liste des projets / contrats-->
 <div class="row">
-    <div class="col">
+<div class="col-sm-9" id="jar">
         <!--Boucle php-->
         <!--Cette partie doit etre dans une boucle pour afficher la liste-->
         <?php foreach ($viewmodel as $value) { ?>
@@ -52,4 +54,36 @@
         </nav>
         
     </div>
+    <div class="col-sm-3" id="contrats">
+        <div class="filtrer">
+            <h4>Filtrer par :</h4>
+            <form>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">date</label>
+                <input type="date" class="form-control" id="exampleFormControlInput1">
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Budget</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                  <option>Choisissez un budget</option>
+                  <?php foreach ($viewmodel as $value) { ?>
+                  <option><?php echo $value['budget_indicatif_prj'] ?></option>
+                  <?php } ?>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">État</label>
+                <select class="form-control" id="exampleFormControlSelect1">
+                  <option>État du contrat</option>
+                  <option>Ouvert</option>
+                  <option>Fermé</option>
+                </select>
+              </div>
+              <button type="submit" class="btn btn-primary">Filtrer</button>
+            </form>
+        </div>
+    </div>
 </div>
+    
+</div>
+</body>
--- MVC/views/user/profil.php
+++ MVC/views/user/profil.php
@@ -6,16 +6,16 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="detail-ese">
-                        <h4><?= $userprofil['nom_entreprise_ut']; ?></h4>
-                        <h5><?= $userprofil['prenom_ut'].' '.$userprofil['nom_ut']; ?></h5>
+                        <h4><?=$userprofil['nom_entreprise_ut'];?></h4>
+                        <h5><?=$userprofil['prenom_ut'] . ' ' . $userprofil['nom_ut'];?></h5>
                         <div></div>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="detail-ese">
-                        <div class="ville"><i class="fas fa-map-marker-alt"></i><?= 'Adresse: '. $userprofil['adresse_ut'].', '.$userprofil['nom_ville']; ?></div>
-                        <div class="rate" title="Nombre de vote"><i class="fas fa-phone"></i><?= 'Téléphone: '.$userprofil['telephone_ut']; ?> </div>
-                        <div class="taux"><i class="fas fa-briefcase"></i><?= 'NEQ: '.$userprofil['neq_ut']; ?></div>
+                        <div class="ville"><i class="fas fa-map-marker-alt"></i><?='Adresse: ' . $userprofil['adresse_ut'] . ', ' . $userprofil['nom_ville'];?></div>
+                        <div class="rate" title="Nombre de vote"><i class="fas fa-phone"></i><?='Téléphone: ' . $userprofil['telephone_ut'];?> </div>
+                        <div class="taux"><i class="fas fa-briefcase"></i><?='NEQ: ' . $userprofil['neq_ut'];?></div>
                     </div>
                     <!-- modifier son profile -->
                     <div class="icon-update"><a href="#"><i class="fas fa-pen-square"></i></a></div>
@@ -23,7 +23,7 @@
             </div>
             <div class="info-plus">
                 <h5>A propos</h5>
-                <div><?= $userprofil['apropos_ut']; ?></div>
+                <div><?=$userprofil['apropos_ut'];?></div>
             <div class="info-plus">
             <div id="accordion" role="tablist">
                 <div role="tab" id="headingOne">
@@ -33,12 +33,12 @@
                     </a>
                   </h5>
                 </div>
-                
+
                   <!--LA BOUCLE COMMENCE PAR ICI-->
-                  <?php $old_id_soumission ='';
-                   foreach($projects as $soumission): ?>
-                    <?php if($soumission['id_prj'] != $old_id_soumission): ?>
-                    <?php if($old_id_soumission != ''): ?>
+                  <?php $old_id_soumission = '';
+foreach ($projects as $soumission): ?>
+                    <?php if ($soumission['id_prj'] != $old_id_soumission): ?>
+                    <?php if ($old_id_soumission != ''): ?>
                                     </tbody>
                                   </table> <!-- soumissionnaires -->
                                 </div> <!-- id="collapseTwo" -->
@@ -48,25 +48,25 @@
                     </tbody>
                   </table> <!-- contrats -->
                   </div>
-                  <?php endif; ?>
+                  <?php endif;?>
                   <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
                   <table class="table tab-index">
                     <tbody>
-                      <tr style="border:none"> 
-                        <th class="tab-titre" style="border:none"><a href="view/fournisseur/detail_contrats.php"><?= $soumission['titre_prj'] ?></a></th>
+                      <tr style="border:none">
+                        <th class="tab-titre" style="border:none"><a href="view/fournisseur/detail_contrats.php"><?=$soumission['titre_prj']?></a></th>
                         <th style="text-align:right">
                             <button type="submit" id="modifer_prj" class="btn-crud">
-                                <a href="<?php echo ROOT_URL; ?>contract/update/"<?= $soumission['id_prj'] ?>>Modifier</a>
+                                <a href="<?php echo ROOT_URL; ?>contract/update/"<?=$soumission['id_prj']?>>Modifier</a>
                             </button>
                             <button type="submit" id="supprimer_prj" class="btn-crud-delete">
-                                <a href="<?php echo ROOT_URL; ?>contract/delete/"<?= $soumission['id_prj'] ?>>Supprimer</a>
+                                <a href="<?php echo ROOT_URL; ?>contract/delete/"<?=$soumission['id_prj']?>>Supprimer</a>
                             </button>
-                        </th>                        
+                        </th>
                       </tr>
                       <tr>
-                        <td>Soumissionnaires: <strong><?= !empty($soumission['comptage']) ? $soumission['comptage'] : 0; ?></strong>
+                        <td>Soumissionnaires: <strong><?=!empty($soumission['comptage']) ? $soumission['comptage'] : 0;?></strong>
                           <br>
-                          Budget de <strong><?= $soumission['budget_indicatif_prj']; ?> $</strong></td>
+                          Budget de <strong><?=$soumission['budget_indicatif_prj'];?> $</strong></td>
                         <td style="text-align:right">État du contrat : <span class="contrat-ouvert">OUVERT</span></td>
                       </tr>
                       <tr>
@@ -80,45 +80,90 @@
                                   </h5>
                                 </div>
                                 <?php
-                                // $soum = new SoumissionModel()
-                                // $soumissions = $soum->getSoumissionsParProjet($soumission['id_prj']);
-                                // foreach(){};
-                                ?>
+// $soum = new SoumissionModel()
+// $soumissions = $soum->getSoumissionsParProjet($soumission['id_prj']);
+// foreach(){};
+?>
                                 <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTWO" data-parent="#accordion2">
                                   <table class="table tab-index">
                                     <tbody>
-                                    <?php endif; ?>
+                                    <?php endif;?>
                                       <tr style="border:none">
-                                        <th class="tab-titre" style="border:none"><a href="#"><?= $soumission['prenom_ut'].' '.$soumission['nom_ut']; ?></a></th>
-                                          <td><?= $soumission['titre_profil_ut']; ?></td>
+                                        <th class="tab-titre" style="border:none"><a href="#"><?=$soumission['prenom_ut'] . ' ' . $soumission['nom_ut'];?></a></th>
+                                          <td><?=$soumission['titre_profil_ut'];?></td>
                                           <td style="text-align:right">
-                                              <button type="submit" id="detail" class="btn-crud" data-toggle="modal" data-target="#ModalDetail">
-                                                  <a href="<?php echo ROOT_URL; ?>user/profil/"<?=$soumission['id_ut'] ?>>detail</a>
+                                              <button type="submit" id="detail" class="btn-crud" data-toggle="modal" data-target="#ModalDetail<?=$soumission['id_ut']?>">
+                                                  <a href="javascript:void(0)">detail</a>
                                               </button>
                                               <button type="submit" id="choisir" class="btn-crud-add">
                                                   <a href="#">Choisir ce candidat</a>
                                               </button>
                                           </td>
                                       </tr>
-                  <?php $old_id_soumission = $soumission['id_prj']; ?>
-                 <?php endforeach; ?>
+                  <?php $old_id_soumission = $soumission['id_prj'];?>
+                 <?php endforeach;?>
                                     </tbody>
                                   </table>
                                 </div>
                             </div>
                         </td>
                       </tr>
-                     
+
                     </tbody>
                   </table>
                 </div>
-                
+
             </div>
-            
+
             </div>
         </div>
-    </div>  
+    </div>
 
-    
+
 </div>
-   
+
+<!-- ModalDetail Freelancer -->
+<div class="container" id="pigiste">
+<?php foreach ($projects as $soumission): ?>
+  <?php if (!empty($soumission['id_soum'])): ?>
+    <div class="modal fade" id="ModalDetail<?=$soumission['id_ut']?>" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header detail">
+            <h4 class="modal-title" id="exampleModalLongTitle"><?=$soumission['prenom_ut'] . ' ' . $soumission['nom_ut'];?></h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body detail-modal">
+
+            <h5>A propos</h5>
+            <div><?=$soumission['apropos_ut'];?></div>
+
+            <div class="detail-modal">
+                <h5>Compétences</h5>
+                <ul>
+                    <li><?=$soumission['competence_ut'];?></li>
+                </ul>
+            </div>
+
+            <div class="detail-modal">
+                <h5>Taux horaire</h5>
+                <div><i class="fas fa-dollar-sign"></i><?=$soumission['taux_horaire_ut'];?>/h</div>
+            </div>
+
+            <div class="detail-modal">
+                <h5>Années d'experience</h5>
+                <div><i class="fas fa-graduation-cap"></i><?=$soumission['annee_experience_ut'];?> ans</div>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+      </div>
+      <?php endif;?>
+      <?php endforeach;?>
+</div>
